ffee76c71cab794ab5fedde1b6c3180b3086d46e
Emma Alyx Wunder <hallo@emmalyx.dev>
2015-12-30 17:02:51 +0100

    Initial Commit

---
+++ MinimalMIDIPlayer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MinimalMIDIPlayer
+//
+//  Created by Peter Wunder on 30.12.15.
+//  Copyright © 2015 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
