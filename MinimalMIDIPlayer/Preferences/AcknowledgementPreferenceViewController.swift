eff0ea20fffc6201b1223c3d48acb44da8b4c076
Emma Alyx Wunder <hallo@emmalyx.dev>
2019-03-13 12:39:26 +0100

    Lots of WIPs
    WIP: unconstrained speed slider movement when holding shift
    WIP: acknowledgements tab in the preferences
    WIP: acknowledgements popover in the about window

---
+++ MinimalMIDIPlayer/Preferences/AcknowledgementPreferenceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AcknowledgementPreferenceViewController.swift
+//  MinimalMIDIPlayer
+//
+//  Created by Peter Wunder on 13.03.19.
+//  Copyright © 2019 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+class AcknowledgementPreferenceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
